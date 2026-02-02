5ab578e60e8a6800662277fcf7cef41ed992bbdd
CupcakeIIE <cupcake.ensiie@gmail.com>
2026-02-02 10:55:10 +0100

    [FEAT] create app + grid of images

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Memory from './Memory'
+
+function App() {
+
+  return (
+    <>
+      <Memory />
+    </>
+  )
+}
+
+export default App
---
+++ src/Memory/index.jsx
@@ -0,0 +1,98 @@
+import { Grid } from '@mui/material'
+
+const Memory = () => {
+
+  return (
+    <>
+      {/* ligne 1 */}
+      <Grid container spacing={2}>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+      </Grid>
+    
+      {/* ligne 2 */}
+      <Grid container spacing={2}>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+      </Grid>
+    
+      {/* ligne 3 */}
+      <Grid container spacing={2}>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+      </Grid>
+    
+      {/* ligne 4 */}
+      <Grid container spacing={2}>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+        <Grid size={2}>
+          <img src='syndra.jpg' width='200' />
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export default Memory;
