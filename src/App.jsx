abcc17fb250c7b07e82266c7da8f1737e6cc5ed8
CupcakeIIE <cupcake.ensiie@gmail.com>
2026-02-03 16:03:10 +0100

    [FEAT] add appbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Memory from './Memory'
+import MyAppBar from './AppBar'
 
 function App() {
 
+  const [mode, setMode] = useState(0)
+
+  console.log('mode', mode)
+
   return (
     <>
+      <MyAppBar setMode={setMode} />
       <Memory />
     </>
   )
---
+++ src/AppBar.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+import { IconButton, Toolbar, Typography, AppBar, Drawer, ListItemText, List, ListItemButton } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+
+const MyAppBar = ({setMode}) => {
+  
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const onClickListItemMode = (numero) => {
+    setMode(numero)
+  }
+
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            // onClick={handleDrawerOpen}
+            edge="start"
+            onClick={handleDrawerOpen}
+            sx={{
+              "&:focus": {
+                outline: "none",
+              },
+            }}
+          >
+            <MenuIcon fontSize="large" />
+          </IconButton>
+          <Typography variant="h6" sx={{position: 'absolute', left: '50%'}}>
+            MiniGames
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <IconButton 
+          onClick={handleDrawerClose}
+          sx={{
+            "&:focus": {
+              outline: "none",
+            },
+            marginTop: '1em'
+          }}
+        >
+          <ChevronLeftIcon />
+        </IconButton>
+        <List>
+          <ListItemButton onClick={() => onClickListItemMode(0)}>
+            <ListItemText style={{margin: '2em'}}>Niveau 1</ListItemText>
+          </ListItemButton>
+          <ListItemButton onClick={() => onClickListItemMode(1)}>
+            <ListItemText style={{margin: '2em'}}>Niveau 2</ListItemText>
+          </ListItemButton>
+        </List>
+      </Drawer>
+    </>
+  )
+}
+
+export default MyAppBar;
